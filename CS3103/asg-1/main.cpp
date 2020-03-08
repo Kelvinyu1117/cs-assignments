9e6e2f13cc8e3dd30c8b221ad03b0ac89f8877d6
Kelvinyu <wukuwnyu@gmail.com>
2020-03-08 15:08:11 +0800

    dev

---
+++ CS3103/asg-1/main.cpp
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <list>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <cstring>
+
+using namespace std;
+
+struct ProcessBlock
+{
+    pid_t pid;
+    int status;
+    string fileName;
+    int state; // running: 0, stopping: 1, terminated: -1
+};
+
+list<ProcessBlock> processList;
+
+unsigned int numsOfRunningProcess = 0;
+unsigned int numsOfTerminatedProcess = 0;
+unsigned int numsOfStoppedProcess = 0;
+
+void bgCreate(char *fileArgs[])
+{
+
+    ProcessBlock pb;
+    pb.fileName = fileArgs[0];
+
+    pb.pid = fork();
+    if (pb.pid == 0)
+    { // child
+        if (numsOfRunningProcess >= 3)
+        {
+            int res = kill(getpid(), SIGSTOP);
+
+            if (res)
+                exit(EXIT_FAILURE);
+        }
+
+        if (execvp(fileArgs[0], fileArgs) < 0)
+        {
+            cout << "execvp is failed for process." << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pb.pid < 0) // error
+    {
+        cout << "fork error: " << pb.pid << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (numsOfRunningProcess >= 3) // pid > 0 -> parent
+    {
+        pb.state = 1;
+        numsOfStoppedProcess++;
+    }
+    else
+    {
+        pb.state = 0;
+        numsOfRunningProcess++;
+    }
+    processList.push_back(pb);
+}
+
+void checkProcessStatus()
+{
+    int n = processList.size();
+    for (ProcessBlock &pb : processList)
+    {
+        int waitRes = waitpid(pb.pid, &pb.status, WNOHANG);
+        if (waitRes == pb.pid)
+        {
+            if (WIFEXITED(pb.status) && pb.state != -1)
+            {
+                pb.state = -1;
+                cout << pb.pid << " completed" << endl;
+                numsOfRunningProcess--;
+                numsOfTerminatedProcess++;
+            }
+        }
+
+        if (numsOfRunningProcess < 3)
+        {
+            if (pb.state == 1)
+            {
+                pb.state = 0;
+                cout << pb.pid << " automatically restarted" << endl;
+                kill(pb.pid, SIGCONT);
+                numsOfRunningProcess++;
+            }
+        }
+    }
+}
+
+void bgList()
+{
+    for (ProcessBlock pb : processList)
+    {
+        cout << pb.pid << ": " << pb.fileName.substr(2);
+        if (pb.state == -1)
+        {
+            cout << "(terminated)" << endl;
+        }
+        else if (pb.state == 0)
+        {
+
+            cout << "(running)" << endl;
+        }
+        else if (pb.state == 1)
+        {
+            cout << "(stopped)" << endl;
+        }
+    }
+}
+
+void bgStop(pid_t pid)
+{
+
+    list<ProcessBlock>::iterator it = processList.begin();
+
+    for (; it != processList.end(); ++it)
+    {
+        if (it->pid == pid)
+            break;
+    }
+
+    int res = kill(pid, SIGSTOP);
+
+    if (!res)
+    {
+        if (it->state == -1)
+        {
+            cout << it->pid << " already terminated" << endl;
+        }
+        else if (it->state != 1)
+        {
+            it->state = 1;
+            numsOfStoppedProcess++;
+            numsOfRunningProcess--;
+
+            cout << it->pid << " stopped" << endl;
+
+            ProcessBlock pb = *it;
+            processList.erase(it);
+            processList.push_back(pb);
+        }
+        else if (it->state == 1)
+        {
+            cout << it->pid << " already stopped" << endl;
+        }
+    }
+    else
+    {
+        exit(EXIT_FAILURE);
+    }
+}
+
+void bgKill(pid_t pid)
+{
+    bool isfound = false;
+    list<ProcessBlock>::iterator it2 = processList.begin();
+    for (; it2 != processList.end(); ++it2)
+    {
+        if (it2->pid == pid)
+        {
+            isfound = true;
+            break;
+        }
+    }
+    if (isfound)
+    {
+        int res = kill(pid, SIGSTOP);
+        if (!res)
+        {
+            if (it2->state != -1)
+            {
+                it2->state = -1;
+                numsOfStoppedProcess++;
+                numsOfRunningProcess--;
+                cout << it2->pid << " killed" << endl;
+            }
+            else
+            {
+                cout << it2->pid << " already terminated" << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << pid << " does not exist" << endl;
+    }
+}
+
+void bgExit()
+{
+    for (ProcessBlock &pb : processList)
+    {
+        if (pb.state != -1)
+        {
+            int res = kill(pb.pid, SIGTERM);
+
+            if (!res)
+            {
+                if (pb.state == 1)
+                    numsOfStoppedProcess--;
+                else
+                    numsOfRunningProcess--;
+
+                pb.state = -1;
+
+                cout << pb.pid << " killed" << endl;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    bool hasExit = false;
+    do
+    {
+        checkProcessStatus();
+
+        string s;
+        cout << "BP >";
+        getline(cin, s);
+
+        char *p = strtok((char *)s.c_str(), " \t");
+
+        if (p)
+        {
+            int pos = 0;
+            char *command = p;
+            char *fileArgs[5] = {NULL, NULL, NULL, NULL, NULL};
+
+            while (p = strtok(NULL, " \t"))
+            {
+                fileArgs[pos++] = p;
+            }
+
+            if (!strcmp(command, "bg"))
+            {
+                string fileName = "";
+                
+                if(fileArgs[0][0] != '.' && fileArgs[0][1] != '/')
+                    fileName = "./";
+
+                fileName += fileArgs[0];
+                fileArgs[0] = (char *)fileName.c_str();
+
+                bgCreate(fileArgs);
+            }
+            else if (!strcmp(command, "bglist"))
+            {
+                bgList();
+            }
+            else if (!strcmp(command, "bgstop"))
+            {
+                bgStop(atoi(fileArgs[0]));
+            }
+            else if (!strcmp(command, "bgkill"))
+            {
+                bgKill(atoi(fileArgs[0]));
+            }
+            else if (!strcmp(command, "exit"))
+            {
+                bgExit();
+                hasExit = true;
+            }
+        }
+    } while (!hasExit);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# Background Runner
-The simple command prompt C++ program for running the external executable in a multi-processing way
-
-
-# Objective
-- learnt how to use the system call for controlling prcess
-# Compile and Execute
-The following command builds all the .cpp files and execute the main
-
-```
-./run.sh
-```
+This repository contains all the source code of my programming assignment.
